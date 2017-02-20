c8e128d8994bb4069b1fb7eb02a241c75b8d92e8
Granit <gdedushi5@gmail.com>
2017-02-19 22:16:35 -0500

    Update BWT.java

--- BWT.java
+++ BWT.java
@@ -5,19 +5,93 @@ import java.util.Scanner;
 import java.util.Arrays;
 import java.util.List;
 
-public class BWT {	
+public class BWT 
+{	
+	//***************START OF ENCRYPTION METHODS***************
+
+	/*Move first letter of previous string to end, then that is new string. Do this i times.		
+	@param - source is the input string to start transform
+	@param - s is string array that will store new transformed strings
+	*/
+	public static void genRotations(String source, String[] s)
+	{
+		s[0] = source;
+		for (int i=1; i < source.length(); i++)
+		{
+			s[i] = s[i-1].substring(1) + s[i-1].charAt(0);
+		}
+	}
+	/*print the string array to see transformation
+	@param - s is string array that will be printed
+	@param - SIZE is the number of rows you would like array to be
+	*/
+	public static void printStringArray(String[] s, int SIZE)
+	{
+	//start at 0 index to print all rows
+		for (int i=0; i < SIZE; i++)
+		{
+			System.out.println(s[i]);
+		}
+		System.out.println();
+	}
+	/*find the row that matches to original input so we can decode
+	@param - s is the string you would like to find
+	@param - a is the string array to search through
+	*/
+	public static void findKey(String s, String[] a)
+	{
+		int key;
+		for (int i=0; i < s.length(); i++)
+		{
+			String match = a[i];
+			while (match == s)
+			{
+				key = i;
+				System.out.println("YOUR INDEX KEY IS:----->" + key + "<-----");
+				System.out.println("----------------------------------"
+				+ "-------------------------------------------");
+				return;
+			}
+		}
+	}
+	/*takes last character of each row in final sorted array and creates a word with them	
+	@param - s is string array to search through
+	@param - SIZE is the length of original string
+	@return - result, which is a word made from last letter of each subsequent row
+	*/
+	public static String lastChars(String[] s, int SIZE){
+		String result = "";
+		for (int i=0; i < SIZE; i++){
+			result = result + s[i].charAt(SIZE-1);
+		}
+		return result;
+	}
 	
-	public static void main(String[] args) {
+	//***************START OF DECRYPTION METHODS***************
 	
+	/*inputs the column letter of encString as the corresponding row in array
+	@param - s is the encrypted string
+	@param - a is the empty string array which will lead back to original transform
+	*/
+	public static void moveFront(String s, String[] a)
+	{
+		for (int i = 0; i < s.length(); i++)
+		{
+			a[i] = s.substring(i, i+1) + a[i];
+		}
+	}
+	
+	
+	public static void main(String[] args) 
+	{
 		Scanner in = new Scanner(System.in);
-	
 		System.out.println("Encrypt(Press 1) or Decrypt(Press 2) ");
 		int startTest = in.nextInt();
-
-		//this is to offset empty entry when pulling next input	
-		in.nextLine();
-	//*********************START OF ENCRYPTION CODE**************************************	
-		if (startTest == 1) {
+		in.nextLine();   //this is to offset empty entry when pulling next input	
+	
+		//*********************START OF ENCRYPTION CODE**************************************	
+		if (startTest == 1) 
+		{
 			System.out.println("Enter something you would like to encrypt: ");
 			String inString = in.nextLine();
 			System.out.println("Hello");
@@ -32,12 +106,12 @@ public class BWT {
 
 			//generate the transfromation process and print first stage of transform
 			genRotations(inString, rotations);
-			System.out.println("Encryption process initializing:\n ");
+			System.out.println("\nGenerating Rotations:\n ");
 			printStringArray(rotations, SIZE);
 			
-			//sorts the array of strings and print the sorted array
+			//sorts the array of strings lexicographically and print the sorted array
 			Arrays.sort(rotations);
-			System.out.println("Initializing Mixer:\n");
+			System.out.println("Initializing Sort:\n");
 			printStringArray(rotations, SIZE);
 			
 			//Take last character of each sorted string to get the final transformed string
@@ -50,15 +124,16 @@ public class BWT {
 			findKey(inString, rotations);
 			}
 
-	//***********************START OF DECRYPTION CODE************************************
-		
-		else if (startTest == 2) {
+		//***********************START OF DECRYPTION CODE************************************
+		else if (startTest == 2) 
+		{
 			System.out.println("Enter encrypted string: ");
 			String encString = in.nextLine();
 			System.out.println("Enter key value: ");
 			int encKey = in.nextInt();
 			//Input Validation, index key must be equal to or greater than 0
-			while(encKey < 0){
+			while(encKey < 0)
+			{
 				System.out.println("Your index value must be positive, Enter Index Again.");
 				encKey = in.nextInt();
 			}
@@ -67,19 +142,20 @@ public class BWT {
 			
 			//creates a new array of strings same size as encString
 			String[] revRotations = new String[SIZE];
-			String result;
 			
 			//placeholder to remove null from decrypted string
 			//this will make the sorted array into list which I will then print
 			List<String> remNull;
 			
 			//Runs once for each stage
-			for(int cycles = 0; cycles < encString.length(); cycles++){
+			for(int cycles = 0; cycles < encString.length(); cycles++)
+			{
 				moveFront(encString, revRotations);
 				Arrays.sort(revRotations);
 				remNull = Arrays.asList(revRotations);
 				System.out.println("Stage " + (cycles+1) + "\n");
-				for (int i = 0; i < SIZE; i++){
+				for (int i = 0; i < SIZE; i++)
+				{
 					String temporary = remNull.get(i);
 					int temporaryInt = temporary.length()-4;
 					temporary = temporary.substring(0, temporaryInt);
@@ -99,65 +175,10 @@ public class BWT {
 			System.out.println("-----------------------------------------------------------------------------");
 			
 		}
-		else {
+		else 
+		{
 			System.out.println("Start me up again when you are ready to encrypt or decrypt.");
 		}
-
+		in.close();
 	}
-
-	
-//***************START OF ENCRYPTION METHODS***************
-
-//Move first letter of previous string to end, then that is new string. Do this i times.		
-	public static void genRotations(String source, String[] s){
-		s[0] = source;
-		for (int i=1; i < source.length(); i++){
-			s[i] = s[i-1].substring(1) + s[i-1].charAt(0);
-		}
-		
-	}
-
-	//print the string array to see transformation
-	public static void printStringArray(String[] s, int SIZE){
-	//start at 0 index to print all rows
-		for (int i=0; i < SIZE; i++){
-			System.out.println(s[i]);
-		}
-		System.out.println();
-	}
-
-	//find the row that matches to original input so we can decode
-	public static void findKey(String s, String[] a){
-		int key;
-		for (int i=0; i < s.length(); i++){
-			String match = a[i];
-				while (match == s){
-					key = i;
-					System.out.println("YOUR INDEX KEY IS:----->" + key + "<-----");
-					System.out.println("----------------------------------"
-					+ "-------------------------------------------");
-					return;
-				}
-			}
-	}
-	
-	//takes last character of each sorted string which provides the encrypted string	
-	public static String lastChars(String[] s, int SIZE){
-		String result = "";
-		
-		for (int i=0; i < SIZE; i++){
-			result = result + s[i].charAt(SIZE-1);
-		}
-		return result;
-	}
-	
-	//***************START OF DECRYPTION METHODS***************
-	
-	//inputs the column letter of encString as the corresponding row in array
-	public static void moveFront(String s, String[] a){
-		for (int i = 0; i < s.length(); i++){
-			a[i] = s.substring(i, i+1) + a[i];
-		}
-	}
-	
 }
